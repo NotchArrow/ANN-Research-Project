c268abc5fa17e178bfb9e3550d1733fa99404861
NotchArrow <149845992+NotchArrow@users.noreply.github.com>
2026-01-17 11:41:07 -0500

    Trial Reading & Writing, GPU RAM Optimization

--- Main.py
+++ Main.py
@@ -1,107 +1,151 @@
 import os
-import random
+import time
+from datetime import datetime
 
+import pandas as pd
 import torch
 from torch import nn
+from torchmetrics.classification import MulticlassF1Score
 
-import ModelData
-from ModelData import Datasets, DataType
+import TrialManager
 
-
-# force gpu usage for training and testing models
-device = torch.device("cuda")
-print(f"Using {device} device")
-
-# force deterministic gpu behavior
-os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
-torch.use_deterministic_algorithms(True)
-
-# generate seed
-seed = random.randint(1, 1000000)
-print(f"Using seed: {seed}")
-
-# seed initial weights and shuffling
-torch.manual_seed(seed)
-torch.cuda.manual_seed_all(seed)
-
-# generate dataloaders
-train_dataloader = ModelData.getLoader(dataset=Datasets.MNIST, batch_size=64, data_type=DataType.TRAINING, seed=seed)
-test_dataloader = ModelData.getLoader(dataset=Datasets.MNIST, batch_size=64, data_type=DataType.TESTING, seed=seed)
-for X, y in test_dataloader:
-    print(f"Shape of X [N, C, H, W]: {X.shape}")
-    print(f"Shape of y: {y.shape} {y.dtype}")
-    break
 
 # define model
 class NeuralNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, architecture):
         super().__init__()
         self.flatten = nn.Flatten()
 
         # set model architecture
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10)
-        )
+        self.linear_relu_stack = architecture
 
     def forward(self, x):
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
 
-# output model info and load to gpu
-model = NeuralNetwork().to(device)
-print(model)
-
 # define training loop
 def train(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
+    total_correct = 0
+    total_samples = 0
+    running_loss = 0.0
+    f1_metric = MulticlassF1Score(num_classes=10, average='macro').to(device, non_blocking=True)
+
     model.train()
     for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to(device), y.to(device)
+        X, y = X.to(device, non_blocking=True), y.to(device, non_blocking=True)
 
         # compute prediction error
         pred = model(X)
         loss = loss_fn(pred, y)
+
+        # record metrics
+        predicted = torch.max(pred, 1)[1]
+        total_correct += (predicted == y).sum().item()
+        total_samples += y.size(0)
+        running_loss += loss.item() * y.size(0)
+        f1_metric.update(pred, y)
 
         # backpropagation
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
 
-        if batch % 100 == 0:
-            loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+    epoch_loss = running_loss / len(dataloader.dataset)
+    epoch_accuracy = total_correct / total_samples
+    epoch_f1 = f1_metric.compute().item()
+    return epoch_loss, epoch_accuracy, epoch_f1
 
 # define testing algorithm for verification using testing data
 def test(dataloader, model, loss_fn):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     model.eval()
+
+    f1_metric = MulticlassF1Score(num_classes=10, average='macro').to(device, non_blocking=True)
     test_loss, correct = 0, 0
     with torch.no_grad():
         for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
+            X, y = X.to(device, non_blocking=True), y.to(device, non_blocking=True)
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    f1 = f1_metric.compute().item()
 
-# default optimizer settings, will be changed during experimentation
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+    return test_loss, correct, f1
 
-# run model training/testing for 5 epochs
-epochs = 5
-loss_fn = nn.CrossEntropyLoss()
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model, loss_fn)
-print("Done!")
+if __name__ == "__main__":
+    # force gpu usage for training and testing models
+    device = torch.device("cuda")
+    print(f"Using {device} device")
+    print("Starting...\n")
+
+    # force deterministic gpu behavior
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.use_deterministic_algorithms(True)
+
+    # get next trial to complete and start loop
+    trial_data = TrialManager.getTrial()
+    while trial_data is not None:
+        train_dataloader, test_dataloader, lr, optimizer_type, architecture, seed = trial_data
+
+        # load model to gpu
+        model = NeuralNetwork(architecture=architecture).to(device)
+
+        # setup model optimizer and loss function
+        optimizer = optimizer_type(model.parameters(), lr=lr)
+        loss_fn = nn.CrossEntropyLoss()
+
+        # setup logging
+        runtimes = []
+        training_losses = []
+        training_accuracies = []
+        training_f1 = []
+        testing_losses = []
+        testing_accuracies = []
+        testing_f1 = []
+
+        # run model training/testing for 50 epochs
+        epochs = 50
+        for t in range(epochs):
+            epoch_start_time = time.time()
+
+            train_metrics = train(train_dataloader, model, loss_fn, optimizer)
+            training_losses.append(train_metrics[0])
+            training_accuracies.append(train_metrics[1])
+            training_f1.append(train_metrics[2])
+
+            test_metrics = test(test_dataloader, model, loss_fn)
+            testing_losses.append(test_metrics[0])
+            testing_accuracies.append(test_metrics[1])
+            testing_f1.append(test_metrics[2])
+
+            runtimes.append(time.time() - epoch_start_time)
+
+        # log data
+        dataFrame = pd.read_csv("trials.csv", dtype=float)
+        for index, row in dataFrame.iterrows():
+            if row["Seed"] == 0:
+                startIndex = index
+                break
+
+        for i, index in enumerate(range(startIndex, startIndex + epochs)):
+            row = dataFrame.iloc[index]
+            row["Seed"] = seed
+            row["Runtime (s)"] = runtimes[i]
+            row["Loss (Training)"] = training_losses[i]
+            row["Accuracy (Training)"] = training_accuracies[i]
+            row["F1 Score (Training)"] = training_f1[i]
+            row["Loss (Testing)"] = testing_losses[i]
+            row["Accuracy (Testing)"] = testing_accuracies[i]
+            row["F1 Score (Testing)"] = testing_f1[i]
+
+        dataFrame.to_csv("trials.csv", index=False)
+        print(f"Finished Trial At: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+
+        # get new trial info
+        trial_data = TrialManager.getTrial()
---
+++ TrialManager.py
@@ -0,0 +1,305 @@
+import random
+from datetime import datetime
+
+import pandas as pd
+import torch
+from torch import optim, nn
+from torch.utils.data import TensorDataset, DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+if __name__ == "__main__":
+    print("Generating trials spreadsheet...")
+    data = {}
+
+    headers = ["Batch Size", "Dataset ID", "Learning Rate", "Optimizer ID", "Architecture ID", "Trial Number", "Epoch",
+               "Seed", "Runtime (s)",
+               "Loss (Training)", "Accuracy (Training)", "F1 Score (Training)",
+               "Loss (Testing)", "Accuracy (Testing)", "F1 Score (Testing)"]
+    i = 1
+    for batchSize in [64, 128, 256]:
+        for dataset in [0, 1, 2]: # MNIST, FASHION, CIFAR
+            for learningRate in [0.01, 0.001, 0.0001]:
+                for optimizer in [0, 1]: # SGD, ADAM
+                    for architecture in range(20):
+                        for trial in range(1, 4):
+                            for epoch in range(1, 51):
+                                data[i] = [batchSize, dataset, learningRate, optimizer, architecture, trial, epoch,
+                                           0, 0,
+                                           0, 0, 0,
+                                           0, 0, 0]
+                                i += 1
+
+    dataFrame = pd.DataFrame(
+        data
+    )
+
+    print(dataFrame.T)
+    dataFrame.T.to_csv("new_trials.csv", header=headers, index=False)
+    print("Please rename your spreadsheet to 'trials.csv' to avoid accidental overwrite!")
+
+
+def getTrial():
+    dataFrame = pd.read_csv("trials.csv", dtype=float)
+    rowCount = dataFrame.shape[0]
+
+    for index, row in dataFrame.iterrows():
+        if row["Epoch"] == 1 and row["Seed"] == 0:
+            batchSize = int(row["Batch Size"])
+            datasetID = row["Dataset ID"]
+            learningRate = row["Learning Rate"]
+            optimizerID = row["Optimizer ID"]
+            architectureID = row["Architecture ID"]
+
+            match datasetID:
+                case 0:
+                    train_dataset = datasets.MNIST(root="data", train=True, download=True, transform=ToTensor())
+                    test_dataset = datasets.MNIST(root="data", train=False, download=True, transform=ToTensor())
+                    input_size = 784
+                case 1:
+                    train_dataset = datasets.FashionMNIST(root="data", train=True, download=True, transform=ToTensor())
+                    test_dataset = datasets.FashionMNIST(root="data", train=False, download=True, transform=ToTensor())
+                    input_size = 784
+                case 2:
+                    train_dataset = datasets.CIFAR10(root="data", train=True, download=True, transform=ToTensor())
+                    test_dataset = datasets.CIFAR10(root="data", train=False, download=True, transform=ToTensor())
+                    input_size = 3072
+
+            seed = random.randint(1, 1000000)
+            print("Trial Info:")
+            print(row[:6])
+            print(f"Using seed: {seed}")
+            print(f"Total trial completion: {(100 * index / rowCount):>0.2f}%")
+
+            # seed initial weights and shuffling
+            torch.manual_seed(seed)
+            torch.cuda.manual_seed_all(seed)
+
+            loaded_train = DataLoader(
+                TensorDataset(
+                    torch.stack([img for img, _ in train_dataset]),
+                    torch.tensor([label for _, label in train_dataset])
+                ),
+                batch_size=batchSize,
+                shuffle=True,
+                generator=torch.Generator().manual_seed(seed),
+            )
+            loaded_test = DataLoader(
+                TensorDataset(
+                    torch.stack([img for img, _ in test_dataset]),
+                    torch.tensor([label for _, label in test_dataset])
+                ),
+                batch_size=batchSize,
+                shuffle=False,
+                generator=torch.Generator().manual_seed(seed),
+            )
+
+            match optimizerID:
+                case 0:
+                    optimizer = optim.SGD
+                case 1:
+                    optimizer = optim.Adam
+
+            match architectureID:
+                case 0:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 10)
+                    )
+                case 1:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 20),
+                        nn.ReLU(),
+                        nn.Linear(20, 10)
+                    )
+                case 2:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 128),
+                        nn.ReLU(),
+                        nn.Linear(128, 10)
+                    )
+                case 3:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 512),
+                        nn.ReLU(),
+                        nn.Linear(512, 10)
+                    )
+                case 4:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 32),
+                        nn.ReLU(),
+                        nn.Linear(32, 32),
+                        nn.ReLU(),
+                        nn.Linear(32, 10)
+                    )
+                case 5:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 64),
+                        nn.ReLU(),
+                        nn.Linear(64, 32),
+                        nn.ReLU(),
+                        nn.Linear(32, 10)
+                    )
+                case 6:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 128),
+                        nn.ReLU(),
+                        nn.Linear(128, 128),
+                        nn.ReLU(),
+                        nn.Linear(128, 10)
+                    )
+                case 7:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 256),
+                        nn.ReLU(),
+                        nn.Linear(256, 64),
+                        nn.ReLU(),
+                        nn.Linear(64, 10)
+                    )
+                case 8:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 64),
+                        nn.ReLU(),
+                        nn.Linear(64, 64),
+                        nn.ReLU(),
+                        nn.Linear(64, 64),
+                        nn.ReLU(),
+                        nn.Linear(64, 10)
+                    )
+                case 9:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 128),
+                        nn.ReLU(),
+                        nn.Linear(128, 64),
+                        nn.ReLU(),
+                        nn.Linear(64, 32),
+                        nn.ReLU(),
+                        nn.Linear(32, 10)
+                    )
+                case 10:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 256),
+                        nn.ReLU(),
+                        nn.Linear(256, 256),
+                        nn.ReLU(),
+                        nn.Linear(256, 256),
+                        nn.ReLU(),
+                        nn.Linear(256, 10)
+                    )
+                case 11:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 512),
+                        nn.ReLU(),
+                        nn.Linear(512, 128),
+                        nn.ReLU(),
+                        nn.Linear(128, 32),
+                        nn.ReLU(),
+                        nn.Linear(32, 10)
+                    )
+                case 12:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 64),
+                        nn.ReLU(),
+                        nn.Linear(64, 64),
+                        nn.ReLU(),
+                        nn.Linear(64, 64),
+                        nn.ReLU(),
+                        nn.Linear(64, 64),
+                        nn.ReLU(),
+                        nn.Linear(64, 10)
+                    )
+                case 13:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 128),
+                        nn.ReLU(),
+                        nn.Linear(128, 128),
+                        nn.ReLU(),
+                        nn.Linear(128, 128),
+                        nn.ReLU(),
+                        nn.Linear(128, 128),
+                        nn.ReLU(),
+                        nn.Linear(128, 10)
+                    )
+                case 14:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 256),
+                        nn.ReLU(),
+                        nn.Linear(256, 128),
+                        nn.ReLU(),
+                        nn.Linear(128, 64),
+                        nn.ReLU(),
+                        nn.Linear(64, 32),
+                        nn.ReLU(),
+                        nn.Linear(32, 10)
+                    )
+                case 15:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 512),
+                        nn.ReLU(),
+                        nn.Linear(512, 256),
+                        nn.ReLU(),
+                        nn.Linear(256, 128),
+                        nn.ReLU(),
+                        nn.Linear(128, 64),
+                        nn.ReLU(),
+                        nn.Linear(64, 10)
+                    )
+                case 16:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 32),
+                        nn.ReLU(),
+                        nn.Linear(32, 32),
+                        nn.ReLU(),
+                        nn.Linear(32, 32),
+                        nn.ReLU(),
+                        nn.Linear(32, 32),
+                        nn.ReLU(),
+                        nn.Linear(32, 32),
+                        nn.ReLU(),
+                        nn.Linear(32, 10)
+                    )
+                case 17:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 128),
+                        nn.ReLU(),
+                        nn.Linear(128, 128),
+                        nn.ReLU(),
+                        nn.Linear(128, 128),
+                        nn.ReLU(),
+                        nn.Linear(128, 128),
+                        nn.ReLU(),
+                        nn.Linear(128, 128),
+                        nn.ReLU(),
+                        nn.Linear(128, 10)
+                    )
+                case 18:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 512),
+                        nn.ReLU(),
+                        nn.Linear(512, 256),
+                        nn.ReLU(),
+                        nn.Linear(256, 128),
+                        nn.ReLU(),
+                        nn.Linear(128, 64),
+                        nn.ReLU(),
+                        nn.Linear(64, 32),
+                        nn.ReLU(),
+                        nn.Linear(32, 10)
+                    )
+                case 19:
+                    architecture = nn.Sequential(
+                        nn.Linear(input_size, 1024),
+                        nn.ReLU(),
+                        nn.Linear(1024, 512),
+                        nn.ReLU(),
+                        nn.Linear(512, 256),
+                        nn.ReLU(),
+                        nn.Linear(256, 128),
+                        nn.ReLU(),
+                        nn.Linear(128, 64),
+                        nn.ReLU(),
+                        nn.Linear(64, 10)
+                    )
+
+            print(f"Started Trial At: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}")
+            return loaded_train, loaded_test, learningRate, optimizer, architecture, seed
+    return None
